969130216785e619d8bfb65aa3f732102bec37be
tavisca-asood <asood@tavisca.com>
2018-09-29 18:41:20 +0530

    Implemented an Aerospike Dumper and API

---
+++ AerospikeDumper/AerospikeDumper/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerospikeDumper
+{
+    class Tweet
+    {
+        public string text { get; set; }
+        public string favorited { get; set; }
+        public string favoriteCount { get; set; }
+        public string created { get; set; }
+        public string truncated { get; set; }
+        public string id { get; set; }
+        public string statusSource { get; set; }
+        public string screenName { get; set; }
+        public string retweetCount { get; set; }
+        public string isRetweet { get; set; }
+        public string timestamp { get; set; }
+        public string date { get; set; }
+    }
+}
